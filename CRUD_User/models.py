3049a0687b95795dc916c4855c3d8032a1abc4fd
Shivam <spa293@uclive.ac.nz>
2025-12-29 21:15:09 +1300

    Added Signals

--- CRUD_User/models.py
+++ CRUD_User/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
 
 # Create your models here.
 class User(models.Model):
@@ -6,3 +8,16 @@ class User(models.Model):
   first_name = models.CharField(max_length=30)
   last_name = models.CharField(max_length=30)
   designation = models.CharField(max_length=50)
+
+
+@receiver(post_save , sender=User)
+def create_user_api(sender, instance, created, **kwargs):
+  print("*"*30)
+  print("User object Created")
+  print("*"*30)
+
+@receiver(post_delete, sender=User)
+def delete_user_api(sender, instance, **kwargs):
+  print("*"*30)
+  print("User object Deleted")
+  print("*"*30)
